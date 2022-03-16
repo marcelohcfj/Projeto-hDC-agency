92adc949c7bd640138ab07adec38a1ad9e82f38e
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-03-16 11:54:19 -0300

    l

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('teste');
+$(document).ready(function() {
+
+// Progress bar
+    let containerA = document.getElementById("circleA");
+
+    let circleA = new Progressbar.Circle(containerA, {
+
+        color: '#64DAF9',
+        strokeWidth: 8,
+        duration: 10800,
+        from: { color: '#AAA' },
+        to: { color:'#65DAF9'},
+    
+        step: function(state, circle) {
+            
+            circle.path.setAttribute('stroke', state.color);
+
+            let value = Math.round(circle.value() * 60);
+
+
+    
+        }
+    });
+
+    circleA.animate(1.0);
+
+});
